033157ca3eb2bfc324ec7b1880654a1aee63790b
Андрей <info@bustep.ru>
2023-10-07 18:44:53 +0700

    template

--- docker/doc/download.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = BASE_DIR . 'database/_backup/2023-09-16T010101+0000.sql';
-$filename = '2023-09-16T010101+0000.sql';
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="' . $filename . '"');
-readfile($file);
-exit();
-
--- docker/doc/phpunit.md
+++
@@ -1,6 +0,0 @@
-# PHPUnit
-
-Для macos смотреть видео
-
-
-[https://disk.yandex.ru/d/7rNKWXtb6jU8uA](Инструкция для PHPStorm по настройке Phpunit тестов.mp4)
